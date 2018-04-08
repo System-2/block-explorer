2aa6295aa96dc5825a6dd8e19e7d37ee1dee2cd7
Vladimir Zhelvis <war1lok@yandex.ru>
2018-04-08 16:29:30 +0300

    client build

--- server.js
+++ server.js
@@ -15,6 +15,12 @@ let login = require('./config').dbLogin,
 module.exports.config = {url, dbName}
 
 app.use(
+    function(req, res, next) {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+        next();
+    },
     express.static(__dirname + '/src'),
     bodyParser()
 );
