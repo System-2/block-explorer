d8dc741f6106a5d00d723f971676d3312c69ed8c
Jayzzer <pavel10_pushkarev@mail.ru>
2018-04-08 12:36:22 +0300

    add search by height

--- app/models/helpModel.js
+++ app/models/helpModel.js
@@ -4,6 +4,7 @@ exports.formBlockData = blockInfo => {
         height: blockInfo.header.height,
         time: blockInfo.header.timestamp,
         bits: blockInfo.header.nBits,
+        difficulty: blockInfo.header.difficulty,
         nonce: blockInfo.header.nonce,
         parentId: blockInfo.header.parentId,  
         txs: blockInfo.blockTransactions,
